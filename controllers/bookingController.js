ce5866fcdde4b75021b8b9391af103954fc4dde6
Chao <chaotanggo@gmail.com>
2022-10-18 02:12:55 +0300

    Implemented stripe message alert

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -17,7 +17,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     // success_url: `${req.protocol}://${req.get('host')}/my-tours/?tour=${
     //   req.params.tourId
     // }&user=${req.user.id}&price=${tour.price}`, // as soon as the purchase is successful, the user will be redirect to this url; walkaround: using this query to create a booking object in the database (not secure because anyone who knows this url can get a tour without paying it)
-    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours?alert=booking`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -64,11 +64,8 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
-  console.log(`tour: ${tour}`);
   const user = (await User.findOne({ email: session.customer_email })).id;
-  console.log(`user: ${user}`);
   const price = session.amount_total / 100;
-  console.log(`price: ${price}`);
   // const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
--- public/js/index.js
+++ public/js/index.js
@@ -6,6 +6,7 @@ import { displayMap } from './mapbox';
 import { login, logout, signup } from './login';
 import { updateSettings } from './updateSettings';
 import { bookTour } from './stripe';
+import { showAlert } from './alerts';
 
 // DOM ELEMENTS
 const mapBox = document.getElementById('map');
@@ -82,3 +83,6 @@ if (bookBtn) {
     bookTour(tourId);
   });
 }
+
+const alertMessage = document.querySelector('body').dataset.alert;
+if (alertMessage) showAlert('success', alertMessage, 20);
